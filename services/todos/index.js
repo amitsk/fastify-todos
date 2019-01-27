9bf0f4647e33df230ceb46130e1518cc0cf5eb9e
amit <amitsk1974@gmail.com>
2019-01-26 21:09:21 -0800

    CRUD methods added

--- services/todos/index.js
+++ services/todos/index.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 // module.exports = function (fastify, opts, next) {
 //   fastify.get('/todos', function (request, reply) {
@@ -10,8 +10,56 @@
 
 //If you prefer async/await, use the following
 
-module.exports = async function (fastify, opts) {
-  fastify.get('/todos', async function (request, reply) {
-    return 'this is an example'
-  })
-}
+const opts = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          name: { type: "string" },
+          description: { type: "string" },
+          completed: { type: "boolean" }
+        }
+      }
+    }
+  }
+};
+
+module.exports = async function(fastify, opts) {
+  fastify
+    .get("/todos/:todoId", async function(request, reply) {
+      request.log.info(`TodoId passed in is  ${request.params.todoId}`);
+      // reply.log.info( request.query)
+      // reply.log.info("Hello")
+      reply.header("Content-Type", "application/json").code(200);
+      return {
+        id: request.params.todoId,
+        name: "My Todo",
+        description: "Descr",
+        completed: false
+      }
+    })
+    .post("/todos", opts, async (request, reply) => {
+      reply.header("Content-Type", "application/json")
+      .code(200);
+      return {
+        id: request.params.todoId,
+        name: "My Todo",
+        description: "Descr",
+        completed: false
+      }
+    })
+    .put("/todos/:todoId", opts, async (request, reply) => {
+      reply.header("Content-Type", "application/json")
+      .code(201)
+      return {
+        id: request.params.todoId,
+        name: "My Todo",
+        description: "Descr",
+        completed: false
+      }
+    })
+    .delete("/todos/:todoId", async (request, reply) => {
+      reply.code(204);
+    });
+};
