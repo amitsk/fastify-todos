fb0f0d8c836b10ad0da419eec34d1757624fdd65
amit <amitsk1974@gmail.com>
2019-01-27 17:58:47 -0800

    Changes for Post & GET

--- services/todos/index.js
+++ services/todos/index.js
@@ -1,14 +1,8 @@
 "use strict";
 
-// module.exports = function (fastify, opts, next) {
-//   fastify.get('/todos', function (request, reply) {
-//     reply.send('this is an example')
-//   })
-
-//   next()
-// }
-
-//If you prefer async/await, use the following
+const loki = require("lokijs");
+const db = new loki("todos.json", { env: "NODEJS", autosave: true });
+const todos = db.addCollection("todos");
 
 const opts = {
   schema: {
@@ -29,35 +23,35 @@ module.exports = async function(fastify, opts) {
   fastify
     .get("/todos/:todoId", async function(request, reply) {
       request.log.info(`TodoId passed in is  ${request.params.todoId}`);
-      // reply.log.info( request.query)
-      // reply.log.info("Hello")
-      reply.header("Content-Type", "application/json").code(200);
-      return {
-        id: request.params.todoId,
-        name: "My Todo",
-        description: "Descr",
-        completed: false
+      let todo = todos.get(request.params.todoId);
+      if (todo === null) {
+        reply.code(404).send();
+      } else {
+        reply.header("Content-Type", "application/json").code(200);
+        return Object.assign({}, todo, {
+          $loki: undefined,
+          meta: undefined,
+          id: todo.$loki
+        });
       }
     })
     .post("/todos", opts, async (request, reply) => {
-      reply.header("Content-Type", "application/json")
-      .code(200);
-      return {
-        id: request.params.todoId,
-        name: "My Todo",
-        description: "Descr",
-        completed: false
-      }
+      reply.header("Content-Type", "application/json").code(200);
+      let newTodo = todos.insert(request.body);
+      return Object.assign({}, newTodo, {
+        $loki: undefined,
+        meta: undefined,
+        id: newTodo.$loki
+      });
     })
     .put("/todos/:todoId", opts, async (request, reply) => {
-      reply.header("Content-Type", "application/json")
-      .code(201)
+      reply.header("Content-Type", "application/json").code(201);
       return {
         id: request.params.todoId,
         name: "My Todo",
         description: "Descr",
         completed: false
-      }
+      };
     })
     .delete("/todos/:todoId", async (request, reply) => {
       reply.code(204);
