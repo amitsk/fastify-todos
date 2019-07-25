51508f0709ef5c85d3336af97feac6709cded0bb
amit <amitsk1974@gmail.com>
2019-07-24 21:52:59 -0700

    updated versions

--- services/root.js
+++ services/root.js
@@ -8,7 +8,7 @@
 //   next()
 // }
 
-//If you prefer async/await, use the following
+// If you prefer async/await, use the following
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
--- services/todos/index.js
+++ services/todos/index.js
@@ -1,103 +1,101 @@
-"use strict";
+'use strict'
 
-const loki = require("lokijs");
+const loki = require('lokijs')
 const db = new loki('todos.db', {
-	autoload: true,
-	autoloadCallback : databaseInitialize,
-	autosave: true, 
-	autosaveInterval: 4000
-});
+  autoload: true,
+  autoloadCallback: databaseInitialize,
+  autosave: true,
+  autosaveInterval: 4000
+})
 
 // implement the autoloadback referenced in loki constructor
-function databaseInitialize() {
-  let entries = db.getCollection("todos");
+function databaseInitialize () {
+  let entries = db.getCollection('todos')
   if (entries === null) {
-    entries = db.addCollection("todos");
+    entries = db.addCollection('todos')
   }
 
   // kick off any program logic or start listening to external events
-  runProgramLogic();
+  runProgramLogic()
 }
 
 // example method with any bootstrap logic to run after database initialized
-function runProgramLogic() {
-  let entryCount = db.getCollection("todos").count();
-  console.log("number of entries in database : " + entryCount);
+function runProgramLogic () {
+  const entryCount = db.getCollection('todos').count()
+  console.log('number of entries in database : ' + entryCount)
 }
 
 const opts = {
   schema: {
     response: {
       200: {
-        type: "object",
+        type: 'object',
         properties: {
-          name: { type: "string" },
-          description: { type: "string" },
-          completed: { type: "boolean" }
+          name: { type: 'string' },
+          description: { type: 'string' },
+          completed: { type: 'boolean' }
         }
       }
     }
   }
-};
+}
 
-
-
-module.exports = async function(fastify, opts) {
+module.exports = async function (fastify, opts) {
   fastify
-    .get("/todos/:todoId", async function(request, reply) {
-      request.log.info(`TodoId passed in is  ${request.params.todoId}`);
-      let todos = db.getCollection('todos')
-      let todo = todos.get(request.params.todoId);
+    .get('/todos/:todoId', async function (request, reply) {
+      request.log.info(`TodoId passed in is  ${request.params.todoId}`)
+      const todos = db.getCollection('todos')
+      const todo = todos.get(request.params.todoId)
       if (todo === null) {
-        reply.code(404).send();
+        reply.code(404).send()
       } else {
-        reply.header("Content-Type", "application/json").code(200);
+        reply.header('Content-Type', 'application/json').code(200)
         return Object.assign({}, todo, {
           $loki: undefined,
           meta: undefined,
           id: todo.$loki
-        });
+        })
       }
     })
-    .post("/todos", opts, async (request, reply) => {
-      reply.header("Content-Type", "application/json").code(201);
-      let todos = db.getCollection('todos')
-      let newTodo = todos.insert(request.body);
+    .post('/todos', opts, async (request, reply) => {
+      reply.header('Content-Type', 'application/json').code(201)
+      const todos = db.getCollection('todos')
+      const newTodo = todos.insert(request.body)
       return Object.assign({}, newTodo, {
         $loki: undefined,
         meta: undefined,
         id: newTodo.$loki
-      });
+      })
     })
-    .put("/todos/:todoId", opts, async (request, reply) => {
-      reply.header("Content-Type", "application/json").code(200);
-      let todos = db.getCollection('todos')
-      let todo = todos.get(request.params.todoId);
+    .put('/todos/:todoId', opts, async (request, reply) => {
+      reply.header('Content-Type', 'application/json').code(200)
+      const todos = db.getCollection('todos')
+      const todo = todos.get(request.params.todoId)
       if (todo === null) {
-        reply.code(404).send();
+        reply.code(404).send()
       } else {
-        let newTodo = request.body;
+        const newTodo = request.body
         request.log.info(`newTodo : ${JSON.stringify(newTodo)}`)
-        let updatedTodo = Object.assign({}, todo, newTodo);
+        const updatedTodo = Object.assign({}, todo, newTodo)
         request.log.info(`Updated TODO : ${JSON.stringify(updatedTodo)}`)
         todos.update(updatedTodo)
         return Object.assign({}, updatedTodo, {
           $loki: undefined,
           meta: undefined,
           id: todo.$loki
-        });
+        })
       }
     })
-    .delete("/todos/:todoId", async (request, reply) => {
-      let todos = db.getCollection('todos')
-      let todo = todos.get(request.params.todoId);
-      
+    .delete('/todos/:todoId', async (request, reply) => {
+      const todos = db.getCollection('todos')
+      const todo = todos.get(request.params.todoId)
+
       if (todo === null) {
-        reply.code(404).send();
+        reply.code(404).send()
       } else {
-        todos.remove({$loki: request.params.todoId});
-        reply.header("Content-Type", "application/json").code(204);
-        return {};
+        todos.remove({ $loki: request.params.todoId })
+        reply.header('Content-Type', 'application/json').code(204)
+        return {}
       }
-    });
-};
+    })
+}
